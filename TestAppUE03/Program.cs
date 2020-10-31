5493551e22993bc508e242a2708144aa04359276
StefanMayr <72915997+StefanMayr@users.noreply.github.com>
2020-10-31 12:24:22 +0100

    Initial commit

---
+++ TestAppUE03/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int convZahl = 0;
+            string inPutValue = "";
+
+            // do while example 
+            do
+            {
+                Console.Write("Bitte geben Sie eine Zahl ein ");
+                inPutValue = Console.ReadLine();
+            } while (!int.TryParse(inPutValue, out convZahl));
+
+            // this is line is new 
+            // call 
+            MyParamFunc(inPutValue);
+            // cw output with  $ symbol
+            Console.WriteLine($"Sie haben die Zahl {convZahl} eingetragen");
+            // alternatively cw output with {}
+            Console.WriteLine("Sie haben die Zahl {0} eingetragen", convZahl);
+
+            Console.Write("Bitte geben Sie einen Text ein ");
+            inPutValue = Console.ReadLine();
+
+            for (int i = 0; i < inPutValue.Length; i++)
+            {
+                Console.WriteLine($"An der Stelle {i} steht der Wert (Inhalt) {inPutValue[i]}");
+            }
+        }
+
+        static int MyParamFunc(string value)
+        {
+            int convValue = Convert.ToInt32(value);
+            if (convValue == 0)
+            {
+                Console.WriteLine("");
+            }
+            else if(convValue > 0)
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+
+            return 66;
+        }
+        static void MyEmptyFunc()
+        {
+
+        }
+    }
+}
---
+++ TestAppUE03/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestAppUE03")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FH OOE IT GmbH")]
+[assembly: AssemblyProduct("TestAppUE03")]
+[assembly: AssemblyCopyright("Copyright © FH OOE IT GmbH 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f9f31918-5761-45fc-9d48-f8c73e94d818")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
